7b8a8640748989299e470a56f55e05af0ee49d95
Maksym Skliarov <maksym.skliarov@gmail.com>
2019-09-08 18:26:46 +0300

    Created deployment scripts

---
+++ scripts/deploy.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+
+HOST=appdev.academy
+SSH_PORT=1235
+APP_DIRECTORY=/www/apps.appdev.academy
+
+
+ssh -p $SSH_PORT root@$HOST << SCRIPT
+  # Stop nginx server
+  service nginx stop
+  
+  # Go to Rails app directory
+  cd $APP_DIRECTORY
+  
+  # Pull new changes
+  git fetch origin
+  git reset --hard origin/master
+  
+  # Update bundle
+  RAILS_ENV=production bundle install
+  
+  # Run migrations and seed database
+  RAILS_ENV=production rake db:migrate
+  RAILS_ENV=production rake db:seed
+  
+  # Start Nginx server
+  service nginx start
+SCRIPT
